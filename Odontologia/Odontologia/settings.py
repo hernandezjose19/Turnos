3507b3d66a15c2470e0acd30835c2b6929a1c85d
Jose Hernandez <josehernandezoboe@gmail.com>
2020-09-15 18:28:08 -0300

    Se cambia el puerto de mail del 587 al 25

--- Odontologia/Odontologia/settings.py
+++ Odontologia/Odontologia/settings.py
@@ -129,7 +129,7 @@ LOGOUT_REDIRECT_URL = 'Login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "antonioaljorna@gmail.com"
 EMAIL_HOST_PASSWORD = "Amoeloboe123"

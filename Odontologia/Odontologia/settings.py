aac70706c5b048218df268e573d656a7591f72b4
Jose Hernandez <josehernandezoboe@gmail.com>
2020-09-15 18:37:21 -0300

    se cambia email de la configuracion

--- Odontologia/Odontologia/settings.py
+++ Odontologia/Odontologia/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "antonioaljorna@gmail.com"
+EMAIL_HOST_USER = "josehernandezoboe@gmail.com"
 EMAIL_HOST_PASSWORD = "Amoeloboe123"

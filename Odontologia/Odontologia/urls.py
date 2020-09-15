191c8a5fad5d0045b2231e17a9a18c26e7a9a14b
Jose Hernandez <josehernandezoboe@gmail.com>
2020-09-15 18:24:38 -0300

    Se anade ruta de auth.urls en el archivo principal de urls

--- Odontologia/Odontologia/urls.py
+++ Odontologia/Odontologia/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('doctores/', include('Doctores.urls'))
+    path('doctores/', include('Doctores.urls')),
+    path('', include('django.contrib.auth.urls'))
 ]

e0a582ad6eea6cf581d86a2047721924ee4e1a94
Jose Hernandez <josehernandezoboe@gmail.com>
2020-11-02 12:08:13 -0300

    Se hace prueba con llaves foraneas

--- Odontologia/Doctores/models.py
+++ Odontologia/Doctores/models.py
@@ -18,3 +18,15 @@ class TurnosAsignados(models.Model):
     Descripcion = models.TextField(max_length=65535)
     ID_Turno_Disponible = models.IntegerField()
 
+class Profesor(models.Model):
+
+
+    Nombre = models.CharField(max_length=128)
+    Apellido = models.CharField(max_length=128)
+    DNI = models.IntegerField()
+
+class Materia(models.Model):
+
+
+    Nombre = models.CharField(max_length=128)
+    Docente = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True)
